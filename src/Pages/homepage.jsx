e4207d5c72052cb74d5f7e48e44e00f07dd9b740
Akhil <akhileshadam186@gmail.com>
2025-05-29 01:27:27 +0530

    commit 1

--- src/Pages/homepage.jsx
+++ src/Pages/homepage.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { toast, Toaster } from "react-hot-toast";
 
-export default function homepage() {
-  return (
-    <div>
-      Hello wofdnjsknkjv
-      asfdf
-      sdv
-      sdv
-      
-    </div>
-  )
+export default function Homepage() {
+    useEffect(() => {
+        toast.success("Please Wait..!");
+    }, []);
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            <Toaster />
+            <br /><br />kitees an official website for electronics students.<br />
+            kitees.vercel.app is an ecommarce for Electronics components and kits.
+            <br />This website is under development. <br />
+        </div>
+    );
 }
